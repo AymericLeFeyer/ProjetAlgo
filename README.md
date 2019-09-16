993087db4692acb64947d685ad07f76630a73fe0
Aymeric <lefeyer.aymeric@gmail.com>
2019-09-16 20:09:22 +0200

    Ajout SDL Modification README Consignes d'installation Programme pour tester la SDL

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ProjetAlgo
 Projet de Développement en équipe - Bataille Navale / Sudoku / Loto / Poker - C
+
+# Membres de l'équipe
+DEWAGHE Guillaume
+GUERY Angèle
+HELIN Dylan
+HELIN Steven
+LE FEYER Aymeric
+
+# GitHub
+https://github.com/AymericLeFeyer/ProjetAlgo
+
+# Trello
+https://trello.com/b/OqrI19I3
--- main.c
+++ main.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <SDL/SDL.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  int i;
-  for (i=0;i<10;i++){
-    printf("Hello les amis \n");
+  // Variable de la boucle principale
+  int continuer = 1;
+
+  // Initialisation de la SDL
+  if (SDL_Init(SDL_INIT_VIDEO) == -1)
+  {
+    fprintf(stderr, "Erreur d'initialisation de la SDL");
+    exit(EXIT_FAILURE);
   }
-  return 0;
+
+  // Création de la surface
+  SDL_Surface *screen = NULL;
+
+  // Affichage de la fenêtre
+  screen = SDL_SetVideoMode(500 , 500, 32, SDL_SWSURFACE | SDL_DOUBLEBUF);
+
+  // Nom de la fenêtre
+  SDL_WM_SetCaption("Test SDL !", NULL);
+
+  // Event pour la détection des entrées (clavier, souris)
+  SDL_Event event;
+
+  // Boucle principale
+  while (continuer){
+
+    // On regarde l'event
+    SDL_WaitEvent(&event);
+
+    // En fonction de l'event, on fait des actions
+    switch(event.type)
+    {
+      // Si on clique sur la croix, on fenêtre
+      case SDL_QUIT:
+        continuer = 0;
+    }
+
+    // On remplit la surface screen avec la couleur, ici du bleu (le meilleur bleu qui existe sur cette terre)
+    SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 161, 255));
+
+    // On "actualise" le tout
+    SDL_Flip(screen);
+  }
+
+  // On quitte le programme, sans erreur
+  return EXIT_SUCCESS;
 }
