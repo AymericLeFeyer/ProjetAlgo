df71dc8df38d48042defb0a7ff35b95a441f2db9
StevenHelin <StevenHelin@users.noreply.github.com>
2019-10-23 14:22:55 +0200

    mise modifié  pour permettre la possibilité de mise bcp + sur la relance

--- sources/mises.c
+++ sources/mises.c
@@ -28,16 +28,11 @@ void miseTotal(JoueurPoker* j1, JoueurPoker* j2, JoueurPoker* j3, JoueurPoker* j
 }
 
 //mise au jeu , suivre=memesomme , relance= mindouble de la memesomme, coucher=etat a 1
-<<<<<<< HEAD
 void miseJeu (JoueurPoker *j, CentrePlateau* p){
   int choix =0; //1=suivre; 2=relance; 3= se coucher
   int choix2=0; //1=+10;2=+5;3=*2;4=defaut (fera *2 par default)
-=======
-Void miseJeu (JoueurPoker *j, CentrePlateau* p){
-  int choix =0; //1=suivre; 2=relance; 3= se coucher
-  int choix2=; //1=+10;2=+5;3=*2;4=defaut (fera *2 par default)
->>>>>>> 034fc29561c0525c51901a9f06f65ce8a2177d1b
-  scanf("%d Votre choix 1=suivre; 2=relance; 3= se coucher\n",choix );
+  int choix3=0; //1=ok
+  scanf("%d Votre choix 1=suivre; 2=relance; 3= se coucher\n",&choix );
 switch (choix) {
   case 1:
     if (j->argent >= p->mise) {
@@ -46,48 +41,43 @@ switch (choix) {
       break;
     }
   case 2:
-  if (j->argent >= (p->mise*2)) {
-    scanf("%d Votre choix 1=+10; 2=+5; 3= s*2; 4=defaut \n",choix2 );
-    switch (choix2) {
-      case 1:
-      if (j->argent >= ((p->mise*2)+10)) {
-        j->argent -= ((p->mise*2)+10);
-        p->mise += ((p->mise*2)+10);
-      }
-    break;
-      case 2:
-      if (j->argent >= ((p->mise*2)+5)) {
-        j->argent -= ((p->mise*2)+5);
-        p->mise += ((p->mise*2)+5);
-      }
-    break;
-      case 3:
-<<<<<<< HEAD
-      if (j->argent >= (p->mise*4)) {
-        j->argent -= ((p->mise*4));
-        p->mise += ((p->mise*4));
-=======
-      if (j->argent >= (p->mise*4) {
-        j->argent -= ((p->mise*4);
-        p->mise += ((p->mise*4);
->>>>>>> 034fc29561c0525c51901a9f06f65ce8a2177d1b
-      }
-    break;
-      case 4:
-      if (j->argent >= (p->mise*2)) {
-        j->argent -= (p->mise*2);
-        p->mise += (p->mise*2);
-      }
-    break;
-
+  while (choix3 !=1){
+    if (j->argent >= (p->mise*2)) {
+      printf("Votre choix 1=+10; 2=+5; 3= s*2; 4=defaut\n");
+      scanf("%d  \n",&choix2 );
+      switch (choix2) {
+        case 1:
+        if (j->argent >= ((p->mise*2)+10)) {
+          j->argent -= ((p->mise*2)+10);
+          p->mise += ((p->mise*2)+10);
+        }
+        break;
+        case 2:
+        if (j->argent >= ((p->mise*2)+5)) {
+          j->argent -= ((p->mise*2)+5);
+          p->mise += ((p->mise*2)+5);
+          }
+        break;
+        case 3:
+        if (j->argent >= (p->mise*4)) {
+          j->argent -= ((p->mise*4));
+          p->mise += ((p->mise*4));
+        }
+        break;
+        case 4:
+          if (j->argent >= (p->mise*2)) {
+            j->argent -= (p->mise*2);
+            p->mise += (p->mise*2);
+        }
+        break;
+    printf("1 pour valider mise\n" );
+    scanf("%d\n",&choix3);
+    }
   }
-  case 3:
-  j->etat= 1;
-  break;
-}
-
-}
-<<<<<<< HEAD
-}
-=======
->>>>>>> 034fc29561c0525c51901a9f06f65ce8a2177d1b
+    break;
+    }
+    case 3:
+      j->etat= 1;
+    break;
+  }
+  }
