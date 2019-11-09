25106eff81c38134676265c80b28c482028d2697
Angele-Guery <Angele-Guery@users.noreply.github.com>
2019-11-09 19:25:28 +0100

    Correction de bugs

--- sources/grilleLoto.c
+++ sources/grilleLoto.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 //génération d'une grille de loto
-void carteLoto (){
+void carteLoto (int t[9][3]){ //grille loto (vide de base)
   int n=0; //nombre de chiffre total
   int i,j,k;
   int nbNombresLigne; //nb de chiffres sur une ligne
-  int t[9][3]; //grille loto
-  for (i=0; i<9; i++){  //initialisation de la grille a -1 (vide)
+  for (i=0; i<9; i++){  //initialisation de la grille a -1 (aucun nombres)
     for (j=0; j<3; j++){
       t[i][j]=-1;
     }
@@ -21,26 +20,24 @@ void carteLoto (){
          nbNombresLigne++;
       }
     }
-    if(t[i][j]==-1 && nbNombresLigne<15){ //alors on met un chiffre dans la grille
+    if(t[i][j]==-1 && nbNombresLigne<5){ //alors on met un chiffre dans la grille
       //vérifications pour les chiffre aléatoire :
       if(j==0){
-        t[i][j]=(rand()3%1)+10*i;
+        t[i][j]=(rand()%(3-1)+1)+10*i;
         n++;
       }
       else{
         if(j==1){
-          t[i][j]=(rand()7%4)+10*i;
+          t[i][j]=(rand()%(7-4)+4)+10*i;
           n++;
         }
         else{
           if(j==2){
-            t[i][j]=(rand()10%8)+10*i;
+            t[i][j]=(rand()%(10-8)+8)+10*i;
             n++;
           }
         }
       }
-
-
 
     }
 
