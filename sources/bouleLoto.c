e579146f8ecb894568eb8db91ddf971ae300f068
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2019-11-15 13:12:24 +0100

    Création des fonctions pour le tirage aléatoire d'un nombre pour le loto

---
+++ sources/bouleLoto.c
@@ -0,0 +1,51 @@
+#include <time.h>
+
+void initialisationLoto(int tasBoules){
+    int talon[90];
+    //Initialisation de toutes les cartes dans un tableau de cartes temporaire
+    int i;
+    for (i = 0; i <= 89; i++) {
+      talon[i]=i+1
+    }
+    //mélange des cartes dans la vraie pioche
+    shuffleLoto(talon,90,tasBoules);
+}
+
+void shuffleLoto(int* talon,int tailleTalon,int* tasBoules){
+int i,j,k,a=tailleTalon;
+
+for(i=0;i<a;i++){
+
+        j=rand()%tailleTalon;
+        tasBoules[i]=talon[j];
+        for(k=j;k<tailleTalon-1;k++){
+            tasBoules[k]=talon[k+1];
+        }
+        talon[tailleTalon-1]=0;
+        tailleTalon=tailleTalon-1;
+    }
+}
+
+int prendreNombre(int *taille,int* tasBoules){
+  *taille=*taille-1;
+  if(*taille<0){
+    return -1;
+  }else{
+    return tasBoules[taille];
+  }
+}
+
+int nombreDansGrille(int grille [9][3],int nombre){
+  int a=0,i=0,j=0;
+  while (a==0 && i<=8) {
+    j=0;
+    while (a==0 && j<=2) {
+      if(grille[i][j]==nombre){
+        a=1;
+      }
+      j++;
+    }
+    i++;
+  }
+  return a;
+}
