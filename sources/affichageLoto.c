e8f338a5c9182c152549e5f786eabadf019a419e
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2019-11-19 16:27:32 +0100

    affichage des grilles

--- sources/affichageLoto.c
+++ sources/affichageLoto.c
@@ -1,42 +1,66 @@
+#include "../headers/structure.h"
 #include "../headers/affichageLoto.h"
 #include "../headers/interface.h"
-#include "../headers/structure.h"
 #include "../headers/shortcuts.h"
+#include "../headers/constantes.h"
 
 void afficheGrilleLoto(CaseLoto t[9][3],SDL_Surface* screen, int numJoueur){
+  int x=0,y=0;
+  if (numJoueur==2 || numJoueur == 4) {
+    x=735;
+  }
+  if (numJoueur==3 || numJoueur == 4) {
+    y=469;
+  }
   TTF_Font *font = NULL;
   font = TTF_OpenFont(FONT_UBUNTU, 30);
   SDL_Color noir = {0, 0, 0, 0};
   SDL_Surface* texte = NULL;
   int i,j;
-  char valeur[3];
+  char valeur[10];
+  SDL_Rect rectTexte = newRect(x+55,y+3,0,0);
+  if(numJoueur==1){
+      texte = IMG_Load("assets/loto/J1.png");
+  }
+  if(numJoueur==2){
+      texte = IMG_Load("assets/loto/J2.png");
+  }
+  if(numJoueur==3){
+      texte = IMG_Load("assets/loto/J3.png");
+  }
+  if(numJoueur==4){
+      texte = IMG_Load("assets/loto/J4.png");
+  }
+  SDL_BlitSurface(texte, NULL, screen, &rectTexte);
   for (i=0; i<9; i++){
     for(j=0; j<3; j++){
       if(t[i][j].val!=-1){
         sprintf(valeur, "%d", t[i][j].val);
         texte = creerTexte(screen, valeur, noir, font);
-          SDL_Rect rectTexte = newRect(5*(i+1),5*(j+1)+49,0,0);
+          SDL_Rect rectTexte = newRect(x+50*(i+1)+5,y+50*(j)+49+5,0,0);
           SDL_BlitSurface(texte, NULL, screen, &rectTexte);
       }
       else{
         if(t[i][j].val==-1){
-          SDL_Rect rectTexte = newRect(5*(i+1),5*(j+1)+49,0,0);
+          SDL_Rect rectTexte = newRect((x+50*(i+1)),y+50*(j)+49,0,0);
           if(numJoueur==1){
-              texte = IMG_Load("assets/loto/J1.png");
+              texte = IMG_Load("assets/loto/r.png");
           }
           if(numJoueur==2){
-              texte = IMG_Load("assets/loto/J2.png");
+              texte = IMG_Load("assets/loto/b.png");
           }
           if(numJoueur==3){
-              texte = IMG_Load("assets/loto/J3.png");
+              texte = IMG_Load("assets/loto/o.png");
           }
           if(numJoueur==4){
-              texte = IMG_Load("assets/loto/J4.png");
+              texte = IMG_Load("assets/loto/v.png");
           }
         SDL_BlitSurface(texte, NULL, screen, &rectTexte);
         }
       }
 
+      SDL_FreeSurface(texte);
     }
   }
+  //SDL_Flip(screen);
 }
--- sources/grilleLoto.c
+++ sources/grilleLoto.c
@@ -1,4 +1,6 @@
+#include "../headers/structure.h"
 #include "../headers/grilleLoto.h"
+
 
 //génération d'une grille de loto
 void carteLoto (CaseLoto t[9][3]){ //un tableau de case
@@ -8,11 +10,12 @@ void carteLoto (CaseLoto t[9][3]){ //un tableau de case
   for (i=0; i<9; i++){  //initialisation de la grille a -1 (aucun nombres)
     for (j=0; j<3; j++){
       t[i][j].val=-1;
+      t[i][j].jeton=false;
     }
   }
   while (n!=15){
-    //i=rand()%8;
-    //j=rand()%2;
+    i=rand()%9;
+    j=rand()%3;
     nbNombresLigne=0; //Pour vériffier si le nombre de chiffre sur la ligne ne dépasse pas 15
     for(k=0;k<9;k++){
       if(t[k][j].val!=-1){
@@ -23,16 +26,19 @@ void carteLoto (CaseLoto t[9][3]){ //un tableau de case
       //vérifications pour les chiffre aléatoire :
       if(j==0){
         t[i][j].val=(rand()%(3-1)+1)+10*i;
+        t[i][j].jeton=false;
         n++;
       }
       else{
         if(j==1){
           t[i][j].val=(rand()%(7-4)+4)+10*i;
+          t[i][j].jeton=false;
           n++;
         }
         else{
           if(j==2){
             t[i][j].val=(rand()%(10-8)+8)+10*i;
+            t[i][j].jeton=false;
             n++;
           }
         }
