b3028b1d91279b65856392d4418b02395b6fbc21
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2019-11-22 15:56:40 +0100

    réparation bataille navale

--- headers/batailleNavale.h
+++ headers/batailleNavale.h
@@ -1,4 +1,4 @@
-int affichageBatailleNavale(SDL_Surface* screen, JoueurBatailleNavale j1, JoueurBatailleNavale j2);
+int affichageBatailleNavale(SDL_Surface* screen);
 void afficherBateaux(SDL_Surface* screen, JoueurBatailleNavale j);
 void ciblerCase(SDL_Surface* screen, SDL_Surface* c, SDL_Rect pos);
 void afficherBateauxSurLeCote(SDL_Surface* screen, JoueurBatailleNavale j1, JoueurBatailleNavale j2);
