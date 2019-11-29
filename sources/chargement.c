6e964f64c4df5c0696d78dae0ab52227cb79103a
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2019-11-29 15:13:06 +0100

    Sauvegarde et chargement pour les profils fonctionnels

--- sources/chargement.c
+++ sources/chargement.c
@@ -12,11 +12,11 @@ void chargementProfils(tabP p) //on mettra en paramètre l'état du joueur pour 
     for(int i = 0; i < 10; i++)
     {
       fscanf(f, "%s ", p[i].nom);
-      fscanf(f, "%d ", &p[i].scoreNavale);
-      fscanf(f, "%d ", &p[i].scorePoker);
-      fscanf(f, "%d ", &p[i].scoreLoto);
-      fscanf(f, "%d ", &p[i].scoreSudoku);
-      fscanf(f, "%d ", &p[i].scoreLoto);
+      fscanf(f, "%f ", &p[i].scoreNavale);
+      fscanf(f, "%f ", &p[i].scorePoker);
+      fscanf(f, "%f ", &p[i].scoreLoto);
+      fscanf(f, "%f ", &p[i].scoreSudoku);
+      fscanf(f, "%f ", &p[i].scoreTotal);
     }
     fclose(f);
   }
